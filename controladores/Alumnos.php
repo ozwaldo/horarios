c86862cdbf6ea2a140fa57fb6ee984640c6fc653
ozwaldo <ozwaldo@gmail.com>
2019-02-15 13:51:51 -0600

    15/02/2019

---
+++ controladores/Alumnos.php
@@ -0,0 +1,34 @@
+<?php
+// Alumnos.php
+/**
+  Acceder al recurso alumnos
+ * GET
+ * localhost/horarios/alumnos/
+ *
+ Registrar alumnos
+ * POST
+ * localhost/horarios/alumnnos/registro
+ *
+ Acceder al WS
+ * POST
+ * localhost/horarios/alumnos/login
+ */
+class Alumnos
+{
+  const NCONTROL = "nControl";
+  const NOMBRE = "nombre";
+  const A_PATERNO = "a_paterno";
+  const A_MATERNO = "a_materno";
+  const CARRERA = "carrera";
+  const EMAIL = "email";
+  const PASSWORD = "password";
+  const CLAVE_API = "claveApi";
+
+  function __construct()
+  {
+    // code...
+  }
+}
+
+
+?>
