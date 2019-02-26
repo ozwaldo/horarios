d954350d7ac85029dc795cb1aa1be206b65a1761
ozwaldo <ozwaldo@gmail.com>
2019-02-26 14:53:01 -0600

    Agregar datos de TBD

---
+++ bdoo.sql
@@ -0,0 +1,27 @@
+INSERT INTO Departamento (nombreDepartamento)
+VALUES ('Producción') RETURNING REF(Departamento)
+INTO aDepartamento;
+
+
+SELECT * from Departamento;
+
+DELETE FROM Departamento
+WHERE nombreDepartamento = 'Produccción';
+
+INSERT INTO Empleado
+  (nombre, apellido, fecha_contrato, MiembroDe)
+VALUES ('Juan','López',DATE '2019-02-26', aDepartamento);
+
+
+
+CREATE CLASS Empleado (
+	nombre VARCHAR(32)  NOT NULL,
+	apellido VARCHAR(32) NOT NULL,
+	fecha_contrato DATE,
+	MiembroDe REFERENCES (Departamento) CARDINALITY(0,1)
+	INVERSE Departamento.Miembros);
+
+CREATE CLASS Departamento(
+	nombreDepartamento VARCHAR(32),
+	Miembros REFERENCES SET(Empleado) CARDINALITY(0,-1)
+	INVERSE Empleado.MiembroDe);
